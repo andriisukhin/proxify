ef391b46e5ee18b03ec9d5d0efee3dbf3dc05a93
andriisukhin <andreysuhin2@gmail.com>
2020-09-14 13:46:57 +0300

    proxify test

--- src/App.vue
+++ src/App.vue
@@ -3,171 +3,102 @@
     <v-app-bar
       app
       color="primary"
-      dark
     >
-      <div class="d-flex align-center">
-
-      </div>
-
-      <v-spacer></v-spacer>
+      <v-btn
+        @click="restart"
+        x-large
+        color="secondary"
+        v-if="ended"
+      >
+        Restart
+      </v-btn>
     </v-app-bar>
-
     <v-content>
-      <v-container fluid>
-        <v-row>
-          <ul>
-            <li v-for="(message, index) in messages.slice(0, next)" v-bind:key="index" :class="message.owner">{{message.text}}</li>
-          </ul>
-        </v-row>
-        <v-row :style="{position: 'absolute', bottom: 0, left: 0, right: 0, padding: '10px', zIndex: 100, background: '#ffffff'}">
-          <v-col cols="12" md="12">
-            <v-textarea
-                    filled
-                    name="input-7-1"
-                    label="Type your message"
-                    v-model="input"
-            ></v-textarea>
-          </v-col>
-          <v-col cols="12" md="12">
-            <div class="my-2">
-              <v-btn @click="send" x-large :style="{position: 'absolute', bottom: 0, right: 0, background: '#56c8d8'}" color="primary" dark>Send Message</v-btn>
-            </div>
-          </v-col>
-        </v-row>
-      </v-container>
+      <list
+        class="list-container"
+        :list="messages"
+      />
+      <controlers
+        :button-text="started  ? 'Send Message' : `Let's chat`"
+        :disabled-field="!started || ended"
+        :disabled-button="ended"
+        v-model="text"
+        @message="started ? send() : startChat()"
+      />
     </v-content>
   </v-app>
 </template>
 
 <script>
+import { useGetters, useActions, useMutations } from 'vuex-composition-helpers';
+import { ref, watch } from '@vue/composition-api';
+import { eventBus } from './main';
+import List from './components/List';
+import Controlers from './components/Controlers';
 
 export default {
   name: 'App',
-
   components: {
-
+    List,
+    Controlers,
   },
+  setup() {
+    const text = ref('');
 
-  data: () => ({
-    name: '',
-    age: 0,
-    location: '',
-    feeling: '',
-    hobby: '',
-    next: 0,
-    input: '',
-    toChat: [],
-    messages: [
-      {
-        text: "Hi, I'm Peter!",
-        owner: 'him'
-      },
-      {
-        text: "What's your name?",
-        ask: "name",
-        owner: 'him'
-      },
-      {
-        text: "Nice to meet you!",
-        owner: 'him'
-      },
-      {
-        text: "How was your day?",
-        ask: "feeling",
-        owner: 'him'
-      },
-      {
-        text: "Where're you from?",
-        ask: "location",
-        owner: 'him'
-      },
-      {
-        text: "Nice!",
-        owner: 'him'
-      },
-      {
-        text: "How old are you?",
-        ask: "age",
-        owner: 'him'
-      },
-      {
-        text: "What's your favorite hobby?",
-        ask: "hobby",
-        owner: 'him'
-      },
-      {
-        text: "Wow, cool",
-      }
-    ]
-  }),
-  methods: {
-    send() {
-      let active = true
-      while(active) {
+    const { messages, started, ended } = useGetters({
+      messages: 'messages',
+      started: 'started',
+      ended: 'ended'
+    })
 
-        if (typeof this.messages[this.next].ask === 'undefined') {
-          this.next += 1;
-        } else {
-          this.next += 1;
-          if (this.messages[this.next].ask === 'name') {
-            this.name = this.input
-            this.messages.splice(this.next, 0,{
-              text: this.input,
-              owner: 'me'
-            })
-          }
-          active = false;
-        }
-      }
+    const { sendMessage, getMessages, getAllMessages } = useActions({
+      sendMessage: 'sendMessage',
+      getMessages: 'getMessages',
+      getAllMessages: 'getAllMessages',
+    })
+
+    const { start, restart } = useMutations({
+      start: 'start',
+      restart: 'cleanState'
+    });
+
+    const send = () =>  {
+      sendMessage({
+        text: text.value,
+        owner: 'me'
+      })
+      text.value = ''
     }
-  }
+
+    const startChat = () => {
+      start();
+      getAllMessages();
+    }
+
+    watch(messages, () => eventBus.$emit('new-message'));
+
+    eventBus.$on('typed',() => getMessages());
+
+    return {
+      text,
+      messages,
+      send,
+      startChat,
+      started,
+      ended,
+      restart
+    }
+  },
 };
 </script>
 
-<style>
-  ul{
-    list-style: none;
-    margin: 0;
-    padding: 0;
-    position: absolute;
-    left: 0;
-    right: 0;
-    overflow-y: scroll;
-    height:600px;
-    z-index: 0;
-    padding-bottom: 100px;
-  }
-
-  ul li{
-    display:inline-block;
-    clear: both;
-    padding: 20px;
-    border-radius: 30px;
-    margin-bottom: 2px;
+<style lang="scss">
+  html, body {
+    height: 100vh;
+    overflow: hidden!important;
     font-family: Helvetica, Arial, sans-serif;
   }
-
-  .him{
-    background: #eee;
-    float: left;
-  }
-
-  .me{
-    float: right;
-    background: #0084ff;
-    color: #fff;
-  }
-
-  .him + .me{
-    border-bottom-right-radius: 5px;
-  }
-
-  .me + .me{
-    border-top-right-radius: 5px;
-    border-bottom-right-radius: 5px;
-  }
-
-  .me:last-of-type {
-    border-bottom-right-radius: 30px;
+  .list-container {
+    height: calc(100vh - 200px);
   }
 </style>
